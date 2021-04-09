035a28a181c22df3331ed5db777da793333fd6dc
pdccheung <pdccheung@gmail.com>
2021-04-09 17:56:21 -0400

    completed assessment

--- project3assessment/main_app/views.py
+++ project3assessment/main_app/views.py
@@ -1,6 +1,34 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Widget
+from django.db.models import Sum
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    if Widget.objects.all().exists():
+        widgets = Widget.objects.all()
+        count = widgets.aggregate(Sum("quantity"))["quantity__sum"]
+    else:
+        widgets = None
+        count= None
+    
+    return render(request, 'home.html', {
+        "widgets": widgets,
+        "count": count,
+    }
+    )
+
+def delete_widget(request, w_id):
+    print(w_id)
+    w = Widget.objects.get(id=w_id)
+    w.delete()
+    response = redirect("home")
+    return response
+
+def add_widget(request):
+    w = Widget(
+        description=request.POST.get("description"),
+        quantity=request.POST.get("quantity")
+    )
+    w.save()
+    response = redirect("home")
+    return response
--- project3assessment/project3assessment/settings.py
+++ project3assessment/project3assessment/settings.py
@@ -37,6 +37,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # Application definition
 
 INSTALLED_APPS = [
+    'main_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
